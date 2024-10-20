126a709a8d7f135e0123cd1b3d4da6b88fdb8b5b
Jake Cleary <shout@jakecleary.net>
2024-10-20 16:49:26 +0100

    Add nix-darwin based system flake

---
+++ configuration.nix
@@ -0,0 +1,52 @@
+{ pkgs, config, lib, ... }: 
+{
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile
+  environment.systemPackages = with pkgs;
+  [ 
+    git
+    gleam
+    hack-font
+    kitty
+    mkalias
+    neofetch
+    obsidian
+    raycast
+    spotify
+    vim
+    vscode
+  ];
+
+  # # homebrew config
+  # homebrew = {
+  # enable = true;
+  # onActivation.cleanup = "zap";
+  # casks = [
+  #   "bitwarden"
+  #   "google-chrome"
+  #   "whatsapp"
+  # ];
+  # };
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "System flake configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    
+    # homebrew
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+    };
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mini
+    darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
+      
+      system = "aarch64-darwin";
+      
+      modules = 
+      [ 
+        ./configuration.nix
+
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            user = "jake";
+            enableRosetta = true;
+            mutableTaps = false;
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+              "homebrew/homebrew-bundle" = homebrew-bundle;
+            };
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."mini".pkgs;
+  };
+}
