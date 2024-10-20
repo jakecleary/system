6f02cbcf0edb80d24a3edd0bc4ba3ede39a39994
Jake Cleary <shout@jakecleary.net>
2024-10-21 00:59:33 +0100

    home: enable fish, starship, kitty (and configure kitty)

--- home.nix
+++ home.nix
@@ -73,6 +73,52 @@
     autosuggestion.enable = true;
   };
 
+  programs.fish = {
+    enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      # fonts
+      font_family = "Hack";
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+      font_size = 18.0;
+      adjust_line_height = "150%";
+      adjust_column_width = 0;
+      disable_ligatures = "never";
+
+      # theme - gruvbox dark
+      background = "#282828";
+      foreground = "#ebdbb2";
+      cursor = "#928374";
+      selection_foreground = "#928374";
+      selection_background = "#3c3836";
+      color0 = "#282828";
+      color8 = "#928374";
+      color1 = "#cc241d";
+      color9 = "#fb4934";
+      color2 = "#98971a";
+      color10 = "#b8bb26";
+      color3 = "#d79921";
+      color11 = "#fabd2d";
+      color4 = "#458588";
+      color12 = "#83a598";
+      color5 = "#b16286";
+      color13 = "#d3869b";
+      color6 = "#689d6a";
+      color14 = "#8ec07c";
+      color7 = "#a89984";
+      color15 = "#928374";
+    };
+  };
+
   # You can also manage environment variables but you will have to manually
   # source
   #
