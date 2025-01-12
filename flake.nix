e75963617e3d80810f980b4006db88de0419abc3
Jake Cleary <shout@jakecleary.net>
2025-01-12 20:55:56 +0000

    flake: use 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System flake configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     
     nix-darwin = {
@@ -11,7 +11,7 @@
     };
     
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
