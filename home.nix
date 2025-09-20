3dbc67052f653c74e51c7fedaf6ddf78489dc765
Jake Cleary <shout@jakecleary.net>
2025-09-20 17:14:53 +0100

    home: uninstall kitty

--- home.nix
+++ home.nix
@@ -152,60 +152,6 @@ in
     enable = true;
   };
 
-  programs.kitty = {
-    enable = true;
-    package = pkgs.kitty;
-    settings = with config.colorScheme.palette; {
-      # configure kitty to log in to fish on boot
-      shell = "${pkgs.fish}/bin/fish --login --interactive";
-
-      # fonts
-      font_family = "Hack";
-      bold_font = "auto";
-      italic_font = "auto";
-      bold_italic_font = "auto";
-      font_size = "18.0";
-      adjust_line_height = "150%";
-      adjust_column_width = 0;
-      disable_ligatures = "never";
-
-      # theme
-      #######
-      background = "#${base00}";
-      foreground = "#${base05}";
-      selection_background = "#${base05}";
-      selection_foreground = "#${base00}";
-      url_color = "#${base04}";
-      cursor = "#${base05}";
-      cursor_text_color = "#${base00}";
-      active_border_color = "#${base03}";
-      inactive_border_color = "#${base01}";
-      active_tab_background = "#${base00}";
-      active_tab_foreground = "#${base05}";
-      inactive_tab_background = "#${base01}";
-      inactive_tab_foreground = "#${base04}";
-      tab_bar_background = "#${base01}";
-      macos_titlebar_color = "#${base00}";
-      # normal
-      color0 = "#${base00}";
-      color1 = "#${base08}";
-      color2 = "#${base0B}";
-      color3 = "#${base0A}";
-      color4 = "#${base0D}";
-      color5 = "#${base0E}";
-      color6 = "#${base0C}";
-      color7 = "#${base05}";
-      # bright
-      color8 = "#${base03}";
-      color9 = "#${base09}";
-      color10 = "#${base01}";
-      color11 = "#${base02}";
-      color12 = "#${base04}";
-      color13 = "#${base06}";
-      color14 = "#${base0F}";
-      color15 = "#${base07}";
-    };
-  };
 
   programs.vscode = {
     enable = true;
