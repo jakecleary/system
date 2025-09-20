1b6c6f71f7531ae00c698cb871433cb7539bbd45
Jake Cleary <shout@jakecleary.net>
2025-09-20 16:14:56 +0100

    nix: remove services.nix-daemon.enable (obsolete)

--- configuration.nix
+++ configuration.nix
@@ -121,10 +121,6 @@ in
   # Other
   #######
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
